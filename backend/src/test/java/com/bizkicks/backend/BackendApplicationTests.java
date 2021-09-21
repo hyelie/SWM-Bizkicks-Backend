4277c9974e07acabf6362f86393a205a571515d6
정혜일 <hyelie@postech.ac.kr>
2021-09-21 21:51:42 +0900

    develop : change application.properties

--- backend/src/test/java/com/bizkicks/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/bizkicks/backend/BackendApplicationTests.java
@@ -11,26 +11,14 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
-import com.bizkicks.backend.auth.redis.RedisUtil;
 
 
 @SpringBootTest
 @Transactional
 @NoArgsConstructor
 class BackendApplicationTests {
-    @Autowired private RedisUtil redisUtil;
-
-    @Test
-    void asdf(){
-        Long a = Long.valueOf(1);
-        redisUtil.set("k", "v", a);
-
-        String value = redisUtil.get("k");
-
-        Assertions.assertThat("v").isEqualTo(value);
-
-    }
 }
+
 
 
 
