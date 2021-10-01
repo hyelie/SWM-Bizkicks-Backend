9f965bab0e92b6ea16232440dec12c6bee75bf8f
정혜일 <hyelie@postech.ac.kr>
2021-10-01 23:12:28 +0900

    develop : GET /kickboard/location/kiboardID change resource to base64 encoded string

---
+++ backend/src/main/java/com/bizkicks/backend/dto/ImageDto.java
@@ -0,0 +1,14 @@
+package com.bizkicks.backend.dto;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@Builder
+public class ImageDto {
+    String image;
+}
